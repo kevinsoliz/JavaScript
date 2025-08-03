29545bd539c5adb92522b4d498acb1eb963d420b
Kevin Soliz <kevinsoliz10@gmail.com>
2025-08-03 16:51:10 +0200

    Variables

---
+++ GettingStarted/scripts/index.js
@@ -0,0 +1,10 @@
+
+let nombre = "Kevin"; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName = undefined; // no muy común
+let selectedColor = null; //  el usuario no ha elegido ningún color.
+console.log(nombre);
+//No pueden emepezar con numeros (1name)
+//Tampoco space or hyphen (-)
+//Variable por linea
