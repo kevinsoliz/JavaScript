3cd4d45c010ac8c532292da6c4f2698c872454d7
Kevin Soliz <kevinsoliz10@gmail.com>
2025-09-24 12:44:00 +0200

    Continuamos donde lo dejamos

--- GettingStarted/scripts/2_controlFlow.js
+++ GettingStarted/scripts/2_controlFlow.js
@@ -16,3 +16,52 @@ for (let index in colors)
 // for-of
 for (let color of colors)
     console.log(color);
+
+let i = 0;
+while(i <= 10){
+    if(i % 2 === 0){
+        i++;
+        continue; //todo lo que está después se ignora.
+    }
+    console.log(i);
+    i++;
+}
+
+//Ejercicio 1:
+function maximum(a, b){
+    return (a > b) ? a : b;
+}
+
+console.log(maximum(5, 5));
+
+//Ejercicio 2:
+console.log(isLandscape(1920, 1080));
+function isLandscape(width, height){
+    return (width > height);
+}
+
+//Ejercicio 3:
+console.log(fizzBuzz(7));
+
+/*function fizzBuzz(input) {
+    if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz';
+    else if (input % 3 === 0) return 'Fizz';
+    else if (input % 5 === 0) return 'Buzz';
+    else if(isNaN(input)) return 'Not a number';
+    else return input;
+    
+}
+*/
+//Corregido : 
+// Lo primero es evaluar si es un número.
+function fizzBuzz(input) {
+    if (typeof input !== 'number')
+        return NaN;
+    if ((input % 3 === 0 ) && (input % 5 === 0))
+        return 'FizzBuzz';
+    if (input % 3 === 0) 
+        return 'Fizz';
+    if (input % 5 === 0) 
+        return 'Buzz';
+    return input;
+}
