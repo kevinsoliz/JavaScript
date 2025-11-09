f1f5200ccbbeb3e3eb37189344d83f168d28bb89
Kevin Soliz <kevinsoliz10@gmail.com>
2025-11-09 20:05:00 +0100

    Modulos

--- Advanced/Objects/Stack.js
+++ Advanced/Objects/Stack.js
@@ -1,31 +1,5 @@
-let _count = new WeakMap();
 
-class Stack {
-    constructor(){
-        this.stack = [];
-        _count.set(this, this.stack.length);
-    }
-    get count(){
-        return _count.get(this);
-    }
-
-    peek() {
-        if(this.stack.length < 1)
-            throw new Error('Stack is empty');
-
-        return this.stack[this.stack.length - 1];
-    }
-    push(obj) {
-        this.stack.push(obj);
-    }
-
-    pop(){
-        if(this.stack.length < 1)
-            throw new Error('Stack is emptyyyy');
-        return this.stack.pop();
-
-    }
-}
+const Stack = require('./stackM');
 
 const stack = new Stack();
-stack.push('a');
+stack.hello();
---
+++ Advanced/Objects/stackM.js
@@ -0,0 +1,34 @@
+let _count = new WeakMap();
+
+class Stack {
+    constructor(){
+        this.stack = [];
+        _count.set(this, this.stack.length);
+    }
+    get count(){
+        return _count.get(this);
+    }
+
+    peek() {
+        if(this.stack.length < 1)
+            throw new Error('Stack is empty');
+
+        return this.stack[this.stack.length - 1];
+    }
+    push(obj) {
+        this.stack.push(obj);
+    }
+
+    pop(){
+        if(this.stack.length < 1)
+            throw new Error('Stack is emptyyyy');
+        return this.stack.pop();
+
+    }
+
+    hello() {
+        console.log('hello');
+    }
+}
+
+module.exports = Stack;
